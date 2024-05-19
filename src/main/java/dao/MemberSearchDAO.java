f18670d9238e657b34331a7daae312a4d5d0c896
yasshy-jp <3afdet261@gmail.com>
2024-05-19 09:53:57 +0900

    MySQLへ移行完了に伴うコメントアウトの解除

--- src/main/java/dao/MemberSearchDAO.java
+++ src/main/java/dao/MemberSearchDAO.java
@@ -24,11 +24,8 @@ public class MemberSearchDAO extends DAO {
 		while (rs.next()) {
 			member = new Member();
 			member.setId(rs.getInt("MEMBER_ID"));
-			/*
-			DBをMySQLへ変更に伴う一時的なコメントアウト
 			member.setCustomer_id(rs.getString("CUSTOMER_ID"));
 			member.setCard_id(rs.getString("CARD_ID"));
-			*/
 			member.setSimei(rs.getString("SIMEI"));
 		}
 
