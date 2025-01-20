e1508c7c5e94ab15b12e6f3e3be0603348f5663a
yasshy-jp <3afdet261@gmail.com>
2025-01-20 11:57:21 +0900

    カート内商品の個数を変更した時の商品在庫DBとの連動化

--- src/main/java/shopping/CartRecountAction.java
+++ src/main/java/shopping/CartRecountAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import bean.Item;
+import dao.ProductStockRegisterDAO;
 import tool.Action;
 // カート内での数量変更に伴う各種合計データの更新
 public class CartRecountAction extends Action {
@@ -13,29 +14,62 @@ public class CartRecountAction extends Action {
 	public String execute(HttpServletRequest request) throws Exception {
 		
 		HttpSession session = request.getSession();
-		// 数量変更する商品のIDを取得
+		// 数量変更する商品IDを取得
 		int id = Integer.parseInt(request.getParameter("id"));
-		// 数量変更する商品の変更数量を取得
+		// 数量変更する商品数量を取得
 		int recount = Integer.parseInt(request.getParameter("recount"));
-		// 商品(項目Bean)を格納するカート(List)をセッション属性から取得
+		// 商品を格納するカートの取得
 		List<Item> cart = (List<Item>)session.getAttribute("CART");
-		// 現在の合計金額と個数をセッション属性から取得
+		// 現在の合計金額と個数の取得
 		int totalPrice = (int)session.getAttribute("TOTALPRICE");
 		int totalCount = (int)session.getAttribute("TOTALCOUNT");
 		
 		for (Item item : cart) {
 			if (item.getProduct().getId() == id) {
-				// 数量増加時：増加分を加算して個数を更新
+				// 数量増加時
 				if(item.getCount() < recount) {
-					totalPrice += item.getProduct().getPrice() * (recount - item.getCount());
-					totalCount += (recount-item.getCount());
+					int incrementCount = recount - item.getCount();  // 増加個数
+					totalPrice += item.getProduct().getPrice() * incrementCount;
+					totalCount += incrementCount;
+					// カート内商品の個数の更新（"CART"の要素）					
 					item.setCount(recount);
-				// 数量減少時：減少分を減算して個数を更新
-				}else if(item.getCount() > recount){				
-					totalPrice -= item.getProduct().getPrice() * (item.getCount() - recount);
-					totalCount -= item.getCount() - recount;
+					
+					// 商品DBの在庫の更新（在庫減少）
+					int stock = item.getProduct().getStock() - incrementCount;
+					ProductStockRegisterDAO psrdao = new ProductStockRegisterDAO();
+					int line = psrdao.r️egister(id, stock);
+					if (line != 1) return "stock-register-error.jsp";
+					
+					// セッションに保存中の商品（"LIST"の要素）の在庫の更新
+					item.getProduct().setStock(stock);
+					// 動作確認用コード
+					System.out.println("カート内で「" + item.getProduct().getName() + 
+							"」の個数追加により、在庫が「" + item.getProduct().getStock() + "個」に減少。");
+										
+				// 数量減少時
+				}else if(item.getCount() > recount){
+					int decrementCount = item.getCount() - recount;  // 減少個数
+					totalPrice -= item.getProduct().getPrice() * decrementCount;
+					totalCount -= decrementCount;
+					// カート内商品の個数の更新（"CART"の要素）
 					item.setCount(recount);
+					
+					// 商品DBの在庫の更新（在庫復活）
+					int stock = item.getProduct().getStock() + decrementCount;
+					ProductStockRegisterDAO psrdao = new ProductStockRegisterDAO();
+					int line = psrdao.r️egister(id, stock);
+					if (line != 1) return "stock-register-error.jsp";
+					
+					// セッションに保存中の商品（"LIST"の要素）の在庫の更新
+					item.getProduct().setStock(stock);
+					// 動作確認用コード
+					System.out.println("カート内で「" + item.getProduct().getName() + 
+							"」の個数減少により、在庫が「" + item.getProduct().getStock() + "個」に復活。");
+					
+				}else {
+					return "cart.jsp";
 				}
+				
 				// 税込み合計金額を再計算し、各種合計データをスコープへ再格納
 				int totalPrice_taxIn = (int)(totalPrice * 1.1);
 				session.setAttribute("TOTALPRICE_TAXIN", totalPrice_taxIn);
